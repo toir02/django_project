9c333cf807c90ff06eb003d7711b595f426a91f9
toir02 <wend8765@gmail.com>
2023-08-10 23:48:33 +0300

    refactor views

--- catalog/views.py
+++ catalog/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render
 
 
 def homepage(request):
-    return render(request, 'catalog.homepage.html')
+    return render(request, 'catalog/homepage.html')
 
 
 def contact_information(request):
-    return render(request, 'catalog.contact_information')
+    return render(request, 'catalog/contact_information')
