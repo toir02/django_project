e99559fe2e0cc3fdf7f5d927a88a816190e7aa7b
toir02 <wend8765@gmail.com>
2023-08-11 00:51:00 +0300

    added handle query for feedback

--- catalog/views.py
+++ catalog/views.py
@@ -6,4 +6,12 @@ def homepage(request):
 
 
 def contact_information(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        print(f'Имя: {name}\n'
+              f'Телефон: {phone}\n'
+              f'Сообщение: {message}')
+
     return render(request, 'catalog/contact_information.html')
