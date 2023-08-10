0309dedc30a316732c874c6af2f0f92c43bb9e24
toir02 <wend8765@gmail.com>
2023-08-10 23:30:07 +0300

    added app in settings

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'catalog',
 ]
 
 ROOT_URLCONF = 'config.urls'
