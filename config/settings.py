3a36b51e798e26f538f8ddb7837e18a8c02305eb
toir02 <wend8765@gmail.com>
2023-08-16 01:59:49 +0300

    edit database in settings.py

--- config/settings.py
+++ config/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgres_psycopg2',
+        'NAME': 'django_project',
+        'USER': 'postgres',
+        'PASSWORD': 'xsibit12',
+        'HOST': '127.0.0.1',
+        'PORT': 5432,
     }
 }
 
