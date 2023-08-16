72b8a0f3200c7418ca55d1af29cb08e91d78f004
toir02 <wend8765@gmail.com>
2023-08-16 23:27:10 +0300

    edit path in urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from catalog.views import homepage, contact_information
 
 urlpatterns = [
-    path('homepage', homepage),
+    path('', homepage),
     path('contact_information', contact_information)
 ]
