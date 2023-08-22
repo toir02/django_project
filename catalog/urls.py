c305337bd90d01e3bb987383cf8dc05ca467410f
toir02 <wend8765@gmail.com>
2023-08-23 01:17:07 +0300

    added path to product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from catalog.views import homepage, contacts, products
+from catalog.apps import CatalogConfig
+from catalog.views import homepage, contacts, product
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
     path('', homepage, name='homepage'),
     path('contacts/', contacts, name='contacts'),
-    path('products/', products, name='products'),
+    path('product/<int:pk>/', product, name='product'),
 ]
