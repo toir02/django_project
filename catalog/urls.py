453d4f8bdc6ee6aac1b071aca1d839e4caa49aa2
toir02 <wend8765@gmail.com>
2023-08-25 12:38:07 +0300

    edit paths in urlpatterns for cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import homepage, contacts, product
+from catalog.views import ContactsView, ProductDetailView, ProductListView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', homepage, name='homepage'),
-    path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', product, name='product'),
+    path('', ProductListView.as_view(), name='homepage'),
+    path('contacts/', ContactsView.as_view(), name='contacts'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
 ]
