bcbb2bc7e1338a3676fcc58d29ce1821b0e325da
toir02 <wend8765@gmail.com>
2023-08-26 15:14:15 +0300

    added crud for blog

--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,7 @@
-from django.views.generic import ListView, TemplateView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
 
-from catalog.models import Product
+from catalog.models import Product, Blog
 
 
 class ProductListView(ListView):
@@ -15,3 +16,25 @@ class ContactsView(TemplateView):
 class ProductDetailView(DetailView):
     model = Product
     template_name = 'catalog/product.html'
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'description')
+    success_url = reverse_lazy('catalog:blog_list')
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'catalog/blog_list.html'
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'description', 'image')
+    success_url = reverse_lazy('catalog:blog_list')
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('catalog:blog_list')
