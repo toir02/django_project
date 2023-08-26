95bac1c75695e53135e47265001c6830f25f939c
toir02 <wend8765@gmail.com>
2023-08-26 16:00:39 +0300

    refactor urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
     path('blog/', BlogListView.as_view(), name='blog'),
-    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('blog/edit/', BlogUpdateView.as_view(), name='edit_blog'),
-    path('blog/delete/', BlogDeleteView.as_view(), name='delete_blog')
+    path('blog/view/<int:pk>', BlogDetailView.as_view(), name='view_blog'),
+    path('blog/create/<int:pk>', BlogCreateView.as_view(), name='create_blog'),
+    path('blog/edit/<int:pk>', BlogUpdateView.as_view(), name='edit_blog'),
+    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='delete_blog')
 ]
