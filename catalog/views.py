4103e8edcba212f4fe31499536af4a58f63d13fb
toir02 <wend8765@gmail.com>
2023-08-26 19:27:09 +0300

    added method get queryset for filter is_published value

--- catalog/views.py
+++ catalog/views.py
@@ -36,6 +36,11 @@ class BlogCreateView(CreateView):
 class BlogListView(ListView):
     model = Blog
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = Blog
