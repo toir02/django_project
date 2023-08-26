aab718aea3aa693f3c8e21d783553de678148a37
toir02 <wend8765@gmail.com>
2023-08-26 13:43:11 +0300

    added paths to urlpatterns for blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import ContactsView, ProductDetailView, ProductListView
+from catalog.views import *
 
 app_name = CatalogConfig.name
 
@@ -9,4 +9,8 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='homepage'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('blog/', BlogListView.as_view(), name='blog'),
+    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
+    path('blog/edit/', BlogUpdateView.as_view(), name='edit_blog'),
+    path('blog/delete/', BlogDeleteView.as_view(), name='delete_blog')
 ]
