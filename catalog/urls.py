e2072c650bd744297681db4d7d399db1e8bc99f5
toir02 <wend8765@gmail.com>
2023-09-07 01:04:17 +0300

    added paths crud in urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,12 +6,15 @@ from catalog.views import *
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', ProductListView.as_view(), name='homepage'),
+    path('', ProductListView.as_view(), name='product'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
+    path('product/create/', ProductCreateView.as_view(), name='create_product'),
+    path('product/edit/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
+    path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
     path('blog/', BlogListView.as_view(), name='blog'),
-    path('blog/view/<int:pk>', BlogDetailView.as_view(), name='view_blog'),
+    path('blog/view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
     path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('blog/edit/<int:pk>', BlogUpdateView.as_view(), name='edit_blog'),
-    path('blog/delete/<int:pk>', BlogDeleteView.as_view(), name='delete_blog')
+    path('blog/edit/<int:pk>/', BlogUpdateView.as_view(), name='edit_blog'),
+    path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
 ]
