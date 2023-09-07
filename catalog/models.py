6d8204c1d4236a64108eb64936f36a19f19aed5b
toir02 <wend8765@gmail.com>
2023-09-07 11:44:02 +0300

    added new model version

--- catalog/models.py
+++ catalog/models.py
@@ -50,3 +50,17 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
+
+
+class Version(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name='версия')
+    version_number = models.IntegerField(verbose_name='номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='название версии')
+    is_actual = models.BooleanField(verbose_name='признак текущей версии', default=True)
+
+    def __str__(self):
+        return self.version_name
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
