31310dda830604fa2fd5fea833b6ed75ec23c8f9
toir02 <wend8765@gmail.com>
2023-09-07 11:12:04 +0300

    added validation for product name

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,11 +1,21 @@
 from django import forms
+from django.core.exceptions import ValidationError
 
 from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
 
+    validation_list = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
     class Meta:
         model = Product
         fields = '__all__'
         exclude = ('change_date',)
+
+    def clean_product_name(self):
+        cleaned_data = self.cleaned_data['product_name']
+
+        if cleaned_data in self.validation_list:
+            raise ValidationError('Такое название использовать запрещено.')
+        return cleaned_data
