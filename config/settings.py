2fc946e991c5ae19b14915bbc14116f597a31ed4
toir02 <wend8765@gmail.com>
2023-09-11 01:10:08 +0300

    added email settings

--- config/settings.py
+++ config/settings.py
@@ -136,3 +136,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_USER_MODEL = 'users.User'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'noreply@oscarbot.ru'
+EMAIL_HOST_PASSWORD ='AsTSNVv7pun9'
+EMAIL_USE_SSL = True
