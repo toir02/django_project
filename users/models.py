af4f653833b122a3bf8a5fd8c7bb5e6a071c9643
toir02 <wend8765@gmail.com>
2023-09-10 12:37:37 +0300

    model user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    avatar = models.ImageField(upload_to='users/', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
+    country = models.CharField(max_length=40, verbose_name='страна')
