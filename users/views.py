733c031bf2ceed3999efe2d4ca991e07b94fc77f
toir02 <wend8765@gmail.com>
2023-09-10 22:30:19 +0300

    added register view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
 
-# Create your views here.
+from users.forms import RegisterForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = RegisterForm
