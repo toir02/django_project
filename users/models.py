b9cad066f721c8d4985645bd66e4d3a5af18874f
toir02 <wend8765@gmail.com>
2023-09-10 12:43:10 +0300

    added username and email row

--- users/models.py
+++ users/models.py
@@ -5,6 +5,12 @@ from catalog.models import NULLABLE
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
     avatar = models.ImageField(upload_to='users/', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='номер телефона', **NULLABLE)
     country = models.CharField(max_length=40, verbose_name='страна')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
