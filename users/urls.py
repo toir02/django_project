ebec7bca91f51cebafc890ff608fcacab9827e65
toir02 <wend8765@gmail.com>
2023-09-10 22:34:45 +0300

    added import register view

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
+from users.views import RegisterView
 
 app_name = UsersConfig.name
 
