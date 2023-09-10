3dd20c192845983540d2750bf71fa9827368a78a
toir02 <wend8765@gmail.com>
2023-09-11 01:37:36 +0300

    added method form valid to register view

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.views.generic import CreateView
 
 from users.forms import RegisterForm
 from users.models import User
+from users.sevices import send_verification_mail
 
 
 class RegisterView(CreateView):
@@ -10,3 +11,8 @@ class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user_email = self.request.POST.get('email')
+        send_verification_mail(user_email)
+        return super().form_valid(form)
