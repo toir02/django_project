73f07a682e70a28e8ba3f9e7b0346f7be4366c08
toir02 <wend8765@gmail.com>
2023-09-10 21:54:15 +0300

    added urlpatterns

---
+++ users/urls.py
@@ -0,0 +1,10 @@
+from django.contrib.auth.views import LoginView
+from django.urls import path
+
+from users.apps import UsersConfig
+
+app_name = UsersConfig.name
+
+urlpatterns = [
+    path('', LoginView.as_view(), name='login')
+]
