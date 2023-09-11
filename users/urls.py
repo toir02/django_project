d44dd4e0a9ed7fc625ac1a3e96e805d6a3cb7a62
toir02 <wend8765@gmail.com>
2023-09-12 02:13:23 +0300

    added verification path

--- users/urls.py
+++ users/urls.py
@@ -2,11 +2,12 @@ from django.contrib.auth.views import LoginView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView
+from users.views import *
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', RegisterView.as_view(template_name='users/register.html'), name='register'),
+    path('verification/', verification_user, name='verification'),
 ]
