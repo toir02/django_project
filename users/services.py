22b25072bc172c308af8397a07375b590ddd0a18
toir02 <wend8765@gmail.com>
2023-09-12 02:13:07 +0300

    refactor send verification mail function

--- users/services.py
+++ users/services.py
@@ -5,7 +5,6 @@ from django.core.mail import send_mail
 from config import settings
 
 
-def send_verification_mail(user_email):
-    key = random.randint(1000, 9999)
+def send_verification_mail(user_email, key):
     send_mail(subject='Ключ для верификации', message=f'Ваш ключ для верификации аккаунта: {key}',
               from_email=settings.EMAIL_HOST_USER, recipient_list=[user_email])
