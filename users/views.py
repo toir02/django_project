7ba4fa228e61d09b9999924f5d7b89c0ec576c6c
toir02 <wend8765@gmail.com>
2023-09-12 02:13:54 +0300

    refactor form valid method

--- users/views.py
+++ users/views.py
@@ -1,18 +1,22 @@
+import random
+
+from django.shortcuts import get_object_or_404, render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 from users.forms import RegisterForm
 from users.models import User
-from users.sevices import send_verification_mail
+from users.services import send_verification_mail
 
 
 class RegisterView(CreateView):
     model = User
     form_class = RegisterForm
     template_name = 'users/register.html'
-    success_url = reverse_lazy('users:login')
 
     def form_valid(self, form):
+        key = random.randint(1000, 9999)
+        self.request.session['key'] = key
         user_email = self.request.POST.get('email')
-        send_verification_mail(user_email)
+        send_verification_mail(user_email, key)
         return super().form_valid(form)
