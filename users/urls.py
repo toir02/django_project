c3fd46ec03c2bef9981840dec945b3804826f2fd
toir02 <wend8765@gmail.com>
2023-09-12 13:48:59 +0300

    added path to success verification

--- users/urls.py
+++ users/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', RegisterView.as_view(template_name='users/register.html'), name='register'),
     path('verification/', verification_user, name='verification'),
+    path('success_verification/', success_verification, name='success_verification'),
 ]
