831b997f56510c6661ff45229139e3f9e8d8b41e
toir02 <wend8765@gmail.com>
2023-09-13 01:39:48 +0300

    added send new password function

--- users/services.py
+++ users/services.py
@@ -8,3 +8,8 @@ from config import settings
 def send_verification_mail(user_email, key):
     send_mail(subject='Ключ для верификации', message=f'Ваш ключ для верификации аккаунта: {key}',
               from_email=settings.EMAIL_HOST_USER, recipient_list=[user_email])
+
+
+def send_new_password(user_email, password):
+    send_mail(subject='Новый пароль', message=f'Ваш новый пароль: {password}', from_email=settings.EMAIL_HOST_USER,
+              recipient_list=[user_email])
