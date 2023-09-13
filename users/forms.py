b3dba4b2984cf24a23a1e4b7757ea0b560936d59
toir02 <wend8765@gmail.com>
2023-09-13 11:08:52 +0300

    added style form mixin

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,14 @@ from django import forms
 from users.models import User
 
 
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != "available":
+                field.widget.attrs['class'] = 'form-control'
+
+
 class RegisterForm(UserCreationForm):
 
     class Meta:
