8b5ef75a26f68a3584e81a61297b25f025360306
toir02 <wend8765@gmail.com>
2023-09-17 23:13:04 +0300

    added env to settings

--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,15 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -81,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django_project',
         'USER': 'postgres',
-        'PASSWORD': '123',
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
         'HOST': '127.0.0.1',
         'PORT': 5432,
     }
@@ -146,9 +149,11 @@ EMAIL_USE_SSL = True
 LOGIN_REDIRECT_URL = '/'
 LOGIN_URL = '/'
 
+CACHE_ENABLED = os.getenv('CACHE_ENABLED') == 'True'
+
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379",
+        "LOCATION": os.getenv('CACHE_LOCATION'),
     }
 }
