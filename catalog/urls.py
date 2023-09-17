20960c0cfa08ce00e5f09f8df576c7056e65a38a
toir02 <wend8765@gmail.com>
2023-09-17 23:20:59 +0300

    deleted cached to product detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from catalog.apps import CatalogConfig
 from catalog.views import *
@@ -9,7 +8,7 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path('', ProductListView.as_view(), name='product'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
-    path('product/<int:pk>/', cache_page(60)(ProductDetailView.as_view()), name='view_product'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
     path('product/create/', ProductCreateView.as_view(), name='create_product'),
     path('product/edit/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
