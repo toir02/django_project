890c2ac8d267cb526060dccd3fc0413a4e8aaa80
toir02 <wend8765@gmail.com>
2023-09-17 21:13:35 +0300

    added get category set function

---
+++ catalog/services.py
@@ -0,0 +1,17 @@
+from catalog.models import Category
+from django.conf import settings
+from django.core.cache import cache
+
+
+def get_category_set():
+    queryset = Category.objects.all()
+    if settings.CACHE_ENABLED:
+        key = 'categories'
+        cache_data = cache.get(key)
+        if cache_data is None:
+            cache_data = queryset
+            cache.set(key, cache_data)
+
+        return cache_data
+
+    return queryset
