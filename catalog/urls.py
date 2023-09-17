298b97d631209630e28bb1bc2efd849702d6990f
toir02 <wend8765@gmail.com>
2023-09-17 23:23:43 +0300

    added never cache to paths

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.cache import never_cache
 
 from catalog.apps import CatalogConfig
 from catalog.views import *
@@ -9,12 +10,12 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='view_product'),
-    path('product/create/', ProductCreateView.as_view(), name='create_product'),
-    path('product/edit/<int:pk>/', ProductUpdateView.as_view(), name='edit_product'),
+    path('product/create/', never_cache(ProductCreateView.as_view()), name='create_product'),
+    path('product/edit/<int:pk>/', never_cache(ProductUpdateView.as_view()), name='edit_product'),
     path('product/delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
     path('blog/', BlogListView.as_view(), name='blog'),
     path('blog/view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
-    path('blog/create/', BlogCreateView.as_view(), name='create_blog'),
-    path('blog/edit/<int:pk>/', BlogUpdateView.as_view(), name='edit_blog'),
+    path('blog/create/', never_cache(BlogCreateView.as_view()), name='create_blog'),
+    path('blog/edit/<int:pk>/', never_cache(BlogUpdateView.as_view()), name='edit_blog'),
     path('blog/delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_blog')
 ]
